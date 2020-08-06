f551cdc8646a2d1de006b7c4ffc2d173716d78b5
DanielJDufour <daniel.j.dufour@gmail.com>
2020-08-05 23:12:18 -0500

    updated parseMRF

--- src/parseMRF.js
+++ src/parseMRF.js
@@ -1,20 +1,16 @@
-/*
-    Idea: Library that counts the number of tiles need to cover an x y given tile size
-    .. idk code is pretty easy though
-
-*/
-
-const fetch = require('cross-fetch');
 const findTagByName = require('xml-utils/src/find-tag-by-name');
 const findTagByPath = require('xml-utils/src/find-tag-by-path');
-const findTagsByPath = require('xml-utils/src/find-tags-by-path');
 const getAttribute = require('xml-utils/src/get-attribute');
+
+const getAttributeAsFloat = (xml, key) => {
+  const value = getAttribute(xml, key);
+  if (value !== undefined) return Number.parseFloat(value);
+};
 
 const clean = string => {
   if (string.startsWith('<![CDATA[')) {
     string = string.slice(9, -3);
   }
-
   return string;
 };
 
@@ -29,19 +25,19 @@ module.exports = input => {
     const rasterSize = findTagByPath(meta, ['Raster', 'Size']).outer;
     const rasterPageSize = findTagByPath(meta, ['Raster', 'PageSize']).outer;
 
-    const height = Number.parseFloat(getAttribute(rasterSize, 'y'));
-    const width = Number.parseFloat(getAttribute(rasterSize, 'x'));
+    const height = getAttributeAsFloat(rasterSize, 'y');
+    const width = getAttributeAsFloat(rasterSize, 'x');
     const area = height * width;
-    const depth = Number.parseFloat(getAttribute(rasterSize, 'z')) || 1;
-    const numBands = Number.parseFloat(getAttribute(rasterSize, 'c'));
+    const depth = getAttributeAsFloat(rasterSize, 'z') || 1;
+    const numBands = getAttributeAsFloat(rasterSize, 'c');
 
-    const pageHeight = Number.parseFloat(getAttribute(rasterPageSize, 'y'));
-    const pageWidth = Number.parseFloat(getAttribute(rasterPageSize, 'x'));
-    const pageDepth = Number.parseFloat(getAttribute(rasterPageSize, 'z')) || 1;
+    const pageHeight = getAttributeAsFloat(rasterPageSize, 'y');
+    const pageWidth = getAttributeAsFloat(rasterPageSize, 'x');
+    const pageDepth = getAttributeAsFloat(rasterPageSize, 'z') || 1;
 
     // This is the number of tiles across the number of bands
     // so 4 if have 4 separate tiles for each tile area
-    const pageBands = Number.parseFloat(getAttribute(rasterPageSize, 'c'));
+    const pageBands = getAttributeAsFloat(rasterPageSize, 'c');
 
     const result = {
       height,
@@ -84,9 +80,9 @@ module.exports = input => {
         result.rsets.model = model;
       }
 
-      scale = getAttribute(rsets.outer, 'scale');
+      scale = getAttributeAsFloat(rsets.outer, 'scale');
       if (scale) {
-        result.rsets.scale = Number.parseFloat(scale);
+        result.rsets.scale = scale;
       }
 
       if (rsets.inner) {
@@ -105,17 +101,10 @@ module.exports = input => {
     // Bounding box
     const bbox = findTagByPath(meta, ['GeoTags', 'BoundingBox']);
     if (bbox) {
-      const minx = getAttribute(bbox.outer, 'minx');
-      result.xmin = Number.parseFloat(minx);
-
-      const miny = getAttribute(bbox.outer, 'miny');
-      result.ymin = Number.parseFloat(miny);
-
-      const maxx = getAttribute(bbox.outer, 'maxx');
-      result.xmax = Number.parseFloat(maxx);
-
-      const maxy = getAttribute(bbox.outer, 'maxy');
-      result.ymax = Number.parseFloat(maxy);
+      result.xmin = getAttributeAsFloat(bbox.outer, 'minx');
+      result.ymin = getAttributeAsFloat(bbox.outer, 'miny');
+      result.xmax = getAttributeAsFloat(bbox.outer, 'maxx');
+      result.ymax = getAttributeAsFloat(bbox.outer, 'maxy');
     }
 
     const projection = findTagByName(meta, 'Projection');
@@ -135,9 +124,9 @@ module.exports = input => {
       const empty = findTagByName(twms.inner, 'EmptyInfo');
       if (empty) {
         result.twms.empty = {};
-        const offset = getAttribute(empty.outer, 'offset');
+        const offset = getAttributeAsFloat(empty.outer, 'offset');
         if (offset !== undefined && offset !== null) {
-          result.twms.empty.offset = Number.parseFloat(offset);
+          result.twms.empty.offset = offset;
         }
       }
 
@@ -176,13 +165,13 @@ module.exports = input => {
           height: h, // height in pixels
           heightInPages: heightInPagesForOverview,
           pages: numPagesForOverview,
-          offset: overviews.reduce((total, { pages }) => total + pages, numberTotalPages), // starting offset within the idx array
+          offset: overviews.reduce((total, {pages}) => total + pages, numberTotalPages), // starting offset within the idx array
           width: w, // width in pixels
           widthInPages: widthInPagesForOverview,
           scale: {
             height: height / h,
-            width: width / w
-          }
+            width: width / w,
+          },
         });
       }
 
@@ -191,22 +180,19 @@ module.exports = input => {
 
     const options = findTagByName(meta, 'Options');
     if (options) {
-      // Console.log("options:", options);
       const options_ = {};
       options.inner.split(' ').forEach(opt => {
         let [key, value] = opt.split('=');
         if (key === 'LERC_PREC') {
           value = Number.parseFloat(value);
         }
-
         options_[key] = value;
       });
       result.options = options_;
     }
 
-    // Options
-    //   <Options>LERC_PREC=0.5 V2=ON</Options>
-    // do we need to handle for this?
+    // to-do: parse Options like:
+    // <Options>LERC_PREC=0.5 V2=ON</Options>
 
     return {...result, numBands, widthInTiles: numberPagesAcross, heightInTiles: numberPagesDown};
   } catch (error) {
