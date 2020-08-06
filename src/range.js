65be41f7debbe61b32138eb62073c02d3f11ba29
DanielJDufour <daniel.j.dufour@gmail.com>
2020-08-05 22:44:28 -0500

    updated range.js to handle null and undefined

--- src/range.js
+++ src/range.js
@@ -1,1 +1,5 @@
-module.exports = num => [...Array(num).keys()];
+module.exports = num => {
+  if (typeof num === 'undefined') return undefined;
+  else if (num === null) return null;
+  else return [...Array(num).keys()];
+};
