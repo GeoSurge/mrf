b32deef17513a39113e578d3f09272d6ff3da25d
DanielJDufour <daniel.j.dufour@gmail.com>
2021-07-26 18:58:49 -0400

    updated clustering tests to use flug

--- tests/test.clustering.js
+++ tests/test.clustering.js
@@ -1,15 +1,15 @@
-const test = require('ava');
-const cluster = require('../src/cluster')
+const test = require('flug');
+const cluster = require('../src/utils/cluster');
 
-test('clustering', async t => {
+test('clustering', ({ eq }) => {
     const objs = [
         { start: 10, end: 15 },
         { start: 16, end: 52 },
         { start: 3, end: 7 }
     ];
     const ranges = cluster(objs);
-    t.deepEqual(ranges, [
-        { end: 7, objs: [ { end: 7, start: 3, }, ], start: 3 },
-        { end: 52, objs: [ { end: 15, start: 10, }, { end: 52, start: 16, }, ], start: 10 }
+    eq(ranges, [
+        { start: 3, end: 7, objs: [ { end: 7, start: 3, }, ] },
+        { start: 10, end: 52, objs: [ { end: 15, start: 10, }, { end: 52, start: 16, }, ] }
     ]);
 });
