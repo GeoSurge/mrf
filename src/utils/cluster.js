857acd56fbf2293aa33a38bc70d1d2b53310eb9a
DanielJDufour <daniel.j.dufour@gmail.com>
2021-07-26 18:58:35 -0400

    fixed issue where clustering didn't order results when inpt is unordered

---
+++ src/utils/cluster.js
@@ -0,0 +1,27 @@
+// takes in an array of objects with start and end properties
+// matches them into groups if share an integer boundary
+// one begins where the other ends and vice versa
+
+const cluster = objs => {
+  // sort objects in case they are not already sorted
+  objs = Array.from(objs).sort((a, b) => Math.sign(a.end - b.end));
+
+  const first = objs[0];
+  let range = {start: first.start, end: first.end, objs: [first]};
+  const ranges = [range];
+
+  for (let i = 1; i < objs.length; i++) {
+    const obj = objs[i];
+    // see if adjacent to previous range
+    if (obj.start === range.end + 1) {
+      range.objs.push(obj);
+      range.end = obj.end;
+    } else {
+      range = {start: obj.start, end: obj.end, objs: [obj]};
+      ranges.push(range);
+    }
+  }
+  return ranges;
+};
+
+module.exports = cluster;
