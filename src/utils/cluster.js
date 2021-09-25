01833a97a4dda044a66901547f92f326386fb07f
DanielJDufour <daniel.j.dufour@gmail.com>
2021-09-25 17:17:57 -0400

    updated clustering

--- src/utils/cluster.js
+++ src/utils/cluster.js
@@ -2,12 +2,12 @@
 // matches them into groups if share an integer boundary
 // one begins where the other ends and vice versa
 
-const cluster = objs => {
+function cluster (objs) {
   // sort objects in case they are not already sorted
   objs = Array.from(objs).sort((a, b) => Math.sign(a.end - b.end));
 
   const first = objs[0];
-  let range = {start: first.start, end: first.end, objs: [first]};
+  let range = { start: first.start, end: first.end, objs: [first] };
   const ranges = [range];
 
   for (let i = 1; i < objs.length; i++) {
@@ -17,7 +17,7 @@ const cluster = objs => {
       range.objs.push(obj);
       range.end = obj.end;
     } else {
-      range = {start: obj.start, end: obj.end, objs: [obj]};
+      range = { start: obj.start, end: obj.end, objs: [obj] };
       ranges.push(range);
     }
   }
--- tests/test.clustering.js
+++ tests/test.clustering.js
@@ -1,15 +1,22 @@
-const test = require('flug');
-const cluster = require('../src/utils/cluster');
+const test = require("flug");
+const cluster = require("../src/utils/cluster");
 
-test('clustering', ({ eq }) => {
-    const objs = [
-        { start: 10, end: 15 },
-        { start: 16, end: 52 },
-        { start: 3, end: 7 }
-    ];
-    const ranges = cluster(objs);
-    eq(ranges, [
-        { start: 3, end: 7, objs: [ { end: 7, start: 3, }, ] },
-        { start: 10, end: 52, objs: [ { end: 15, start: 10, }, { end: 52, start: 16, }, ] }
-    ]);
+test("clustering", ({ eq }) => {
+  const objs = [
+    { start: 10, end: 15 },
+    { start: 16, end: 52 },
+    { start: 3, end: 7 }
+  ];
+  const ranges = cluster(objs);
+  eq(ranges, [
+    { start: 3, end: 7, objs: [{ end: 7, start: 3 }] },
+    {
+      start: 10,
+      end: 52,
+      objs: [
+        { end: 15, start: 10 },
+        { end: 52, start: 16 }
+      ]
+    }
+  ]);
 });
