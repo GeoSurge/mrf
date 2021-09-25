668942691bcb57e9bee34e7c0bc6ee4e23d841e0
DanielJDufour <daniel.j.dufour@gmail.com>
2021-09-25 17:25:51 -0400

    updated tests/serve.js

--- tests/serve.js
+++ tests/serve.js
@@ -1,16 +1,15 @@
-const serveStatic = require('serve-static')
-const http = require('http');
-const finalhandler = require('finalhandler')
+const serveStatic = require("serve-static");
+const http = require("http");
+const finalhandler = require("finalhandler");
 
 module.exports = port => {
-    const serve = serveStatic('.', {
-        acceptRanges: true
-    })
-      
-    const server = http.createServer(function onRequest (req, res) {
-        serve(req, res, finalhandler(req, res))
-    })
-      
-    server.listen(port);
-};
+  const serve = serveStatic(".", {
+    acceptRanges: true
+  });
 
+  const server = http.createServer(function onRequest(req, res) {
+    serve(req, res, finalhandler(req, res));
+  });
+
+  server.listen(port);
+};
