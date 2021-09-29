80078650e81f742c5d1041442c7ba8182a7710f9
DanielJDufour <daniel.j.dufour@gmail.com>
2021-09-28 22:42:44 -0400

    formatted

--- src/getRange.js
+++ src/getRange.js
@@ -1,4 +1,4 @@
-module.exports = ({idx, i}) => {
-  const {offset, length} = idx[i];
-  return {start: offset, end: offset + length - 1};
+module.exports = ({ idx, i }) => {
+  const { offset, length } = idx[i];
+  return { start: offset, end: offset + length - 1 };
 };
