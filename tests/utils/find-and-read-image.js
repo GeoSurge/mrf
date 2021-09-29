27079f8822d4e49ba69c9f836b5a17bd673f8fa3
DanielJDufour <daniel.j.dufour@gmail.com>
2021-09-28 22:27:41 -0400

    added test util

--- tests/utils/find-and-read-image.js
+++ tests/utils/find-and-read-image.js
@@ -1,5 +1,9 @@
 const findAndRead = require("find-and-read");
 const readim = require("readim");
 
-// returns { height, width, pixels }
-module.exports = filename => readim({ data: findAndRead(filename) });
+// returns { height, width, data }
+module.exports = async filename => {
+  const buffer = findAndRead(filename);
+  const { height, width, pixels } = await readim({ data: buffer });
+  return { data: pixels, height, width };
+};
