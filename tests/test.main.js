7d7a00bd8c28824cca43cb8e00f2dc9e62d4949f
DanielJDufour <daniel.j.dufour@gmail.com>
2021-09-29 16:41:20 -0400

    updated tests

--- tests/test.main.js
+++ tests/test.main.js
@@ -1,29 +1,27 @@
-const fs = require('fs')
-const test = require('ava')
-const jpeg = require('jpeg-js');
-const pngjs = require('pngjs');
-const serve = require('./serve');
+const test = require("flug");
+const serve = require("./serve");
 
-const MRF = require('../src/MRF')
+const MRF = require("../src/MRF");
 
-const PORT = 8080
+const PORT = 8080;
 
-serve(PORT);
+const server = serve(PORT);
 
 const mrf_url = `http://localhost:${PORT}/data/m_3008501_ne_16_1_20171018.mrf`;
 const idx_url = mrf_url.replace(".mrf", ".idx");
 const data_url = mrf_url.replace(".mrf", ".lrc");
 
-test('initializing MRF with url', async t => {
-  const mrf = new MRF({ mrf_url, idx_url, data_url, strict: true })
-  const metadata = await mrf.metadata
-  t.is(metadata.compression, 'LERC')
-  const idx = await mrf.idx
-  const { numBands, pageHeight, pageWidth } = metadata
+test("initializing MRF with url", async ({ eq }) => {
+  const mrf = new MRF({ mrf_url, idx_url, data_url, strict: true });
+  const meta = await mrf.meta;
+  eq(meta.compression, "LERC");
+  const idx = await mrf.idx;
+  const { numBands, pageHeight, pageWidth } = meta;
 
-  const tiles = [{ height: metadata.height, width: metadata.width }].concat(metadata.overviews)
+  const tiles = [{ height: meta.height, width: meta.width }].concat(meta.overviews);
 
-  const numberTiles = tiles.reduce((total, t) => total + (Math.ceil(t.width / pageWidth) * Math.ceil(t.height / pageHeight) * numBands), 0)
-  t.is(idx.length, 1088)
-  t.is(numberTiles, 1088)
-})
+  const numberTiles = tiles.reduce((total, t) => total + Math.ceil(t.width / pageWidth) * Math.ceil(t.height / pageHeight) * numBands, 0);
+  eq(idx.length, 1088);
+  eq(numberTiles, 1088);
+  server.close();
+});
