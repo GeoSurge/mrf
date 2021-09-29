ee3546e046f1df9431e4cb02ea4843af85800420
DanielJDufour <daniel.j.dufour@gmail.com>
2021-09-29 17:02:26 -0400

    updated

--- fix-broken-esri-package.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh -e
-
-# over-ride old ESRI files
-curl https://raw.githubusercontent.com/Esri/lerc/master/OtherLanguages/js/LercDecode.js -o ./node_modules/lerc/LercDecode.js
-
-cd ./node_modules/lerc/
-npm install
-npm run build
