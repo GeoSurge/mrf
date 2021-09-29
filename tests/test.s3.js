d169fb75744631796aee6282e2db94f1c283f65c
DanielJDufour <daniel.j.dufour@gmail.com>
2021-09-29 16:58:24 -0400

    updated s3 test

--- tests/test.s3.js
+++ tests/test.s3.js
@@ -1,55 +1,57 @@
-const fs = require('fs')
-const test = require('ava')
-const jpeg = require('jpeg-js');
-const pixelmatch = require('pixelmatch');
-const { PNG } = require('pngjs');
-const serve = require('./serve');
-const countInvalidPixels = require('./countInvalidPixels');
-const getImageData = require('./getImageData');
-const getImageDataFromPath = require('./getImageDataFromPath');
+const test = require("flug");
+const pixelmatch = require("pixelmatch");
+const { PNG } = require("pngjs");
+const toImageData = require("to-image-data");
 
-const MRF = require('../src/MRF')
+const serve = require("./serve");
+const countInvalidPixels = require("./utils/count-invalid-pixels");
+const findAndReadImage = require("./utils/find-and-read-image");
+const writeImageToDisk = require("./utils/write-image-to-disk");
 
-const PORT = 8088
+const ERROR_THRESHOLD = 0.05;
 
-serve(PORT);
+const MRF = require("../src/MRF");
 
-const AWS = require('aws-sdk');
+const PORT = 8088;
+
+const server = serve(PORT);
+
+const AWS = require("aws-sdk");
 if (!process.env.AWS_ACCESS_KEY_ID) throw "Must set AWS_ACCESS_KEY_ID";
 
 AWS.config.update({
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
 });
 
 const s3 = new AWS.S3();
 
-const sign = key => s3.getSignedUrlPromise('getObject', {
-  Bucket: 'naip-analytic',
-  Key: key,
-  Expires: 60 * 60, // 1 hr,
-  RequestPayer: 'requester'    
-});
-
-test('initializing MRF with url', async t => {
-  const mrf = new MRF({
-    mrf_url: await sign('al/2017/100cm/rgbir/30085/m_3008501_ne_16_1_20171018.mrf'),
-    idx_url: await sign('al/2017/100cm/rgbir/30085/m_3008501_ne_16_1_20171018.idx'),
-    data_url: await sign('al/2017/100cm/rgbir/30085/m_3008501_ne_16_1_20171018.lrc')
+const sign = key =>
+  s3.getSignedUrlPromise("getObject", {
+    Bucket: "naip-analytic",
+    Key: key,
+    Expires: 60 * 60, // 1 hr,
+    RequestPayer: "requester"
   });
 
-  const metadata = await mrf.metadata
-  t.is(metadata.compression, 'LERC')
-  const idx = await mrf.idx
-  const { numBands, pageHeight, pageWidth } = metadata
+test("initializing MRF with url", async ({ eq }) => {
+  const mrf = new MRF({
+    mrf_url: await sign("al/2017/100cm/rgbir/30085/m_3008501_ne_16_1_20171018.mrf"),
+    idx_url: await sign("al/2017/100cm/rgbir/30085/m_3008501_ne_16_1_20171018.idx"),
+    data_url: await sign("al/2017/100cm/rgbir/30085/m_3008501_ne_16_1_20171018.lrc")
+  });
 
-  const tiles = [{ height: metadata.height, width: metadata.width }].concat(metadata.overviews)
+  const meta = await mrf.meta;
+  eq(meta.compression, "LERC");
+  const idx = await mrf.idx;
+  const { numBands, pageHeight, pageWidth } = meta;
 
-  const numberTiles = tiles.reduce((total, t) => total + (Math.ceil(t.width / pageWidth) * Math.ceil(t.height / pageHeight) * numBands), 0)
-  t.is(idx.length, 1088)
-  t.is(numberTiles, 1088)
+  const tiles = [{ height: meta.height, width: meta.width }].concat(meta.overviews);
 
-  const meta = await mrf.metadata;
+  const numberTiles = tiles.reduce((total, t) => total + Math.ceil(t.width / pageWidth) * Math.ceil(t.height / pageHeight) * numBands, 0);
+  eq(idx.length, 1088);
+  eq(numberTiles, 1088);
+
   const requestedHeight = 100;
   const requestedWidth = 100;
   const options = {
@@ -61,21 +63,26 @@ test('initializing MRF with url', async t => {
     height: requestedHeight,
     width: requestedWidth
   };
-  const values = await mrf.getValues(options);
+  const { data: values } = await mrf.getValues(options);
 
-  t.timeout(60 * 1000);
-  t.is(values.length, 4);
-  t.true(values.every(band => band.length === requestedHeight));
-  t.true(values.every(band => band.every(row => row.length === requestedWidth)));
-  t.is(countInvalidPixels(values), 0);
+  eq(values.length, 4);
+  eq(
+    values.every(band => band.length === requestedHeight),
+    true
+  );
+  eq(
+    values.every(band => band.every(row => row.length === requestedWidth)),
+    true
+  );
+  eq(countInvalidPixels(values), 0);
 
-  const imageData = getImageData(values);
-  const expectedImageData = getImageDataFromPath('./data/scaled_100x100.png');
+  const imageData = toImageData(values);
+  writeImageToDisk(imageData, "s3-actual.png");
+  const expectedImageData = await findAndReadImage("scaled_100x100.png");
   const { height: imageHeight, width: imageWidth } = expectedImageData;
 
-  const diff = new PNG({height: imageHeight, width: imageWidth});    
-  const count = pixelmatch(imageData, expectedImageData.data, diff.data, imageWidth, imageHeight, { threshold: 0.25 });
-
-  // less than 1 percent of pixels wrong
-  t.is(count, 0);
-})
+  const diff = new Uint8ClampedArray(4 * imageHeight * imageWidth);
+  const count = pixelmatch(imageData.data, expectedImageData.data, diff, imageWidth, imageHeight, { threshold: 0.25 });
+  eq(count / (imageHeight * imageWidth) < ERROR_THRESHOLD, true);
+  server.close();
+});
