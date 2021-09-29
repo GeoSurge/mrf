da3e66cb86d3417aeddff9aee10bb191b75c1932
DanielJDufour <daniel.j.dufour@gmail.com>
2021-09-29 17:04:20 -0400

    updated index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
-const MRF = require('./MRF.js');
-const parseIDX = require('./parseIDX.js');
-const parseMRF = require('./parseMRF.js');
-const getTile = require('./getTile.js');
+const MRF = require("./MRF.js");
+const parseIDX = require("./parseIDX.js");
+const parseMRF = require("./parseMRF.js");
 
 module.exports = {
   parseIDX,
   parseMRF,
-  getTile,
-  MRF,
+  MRF
 };
